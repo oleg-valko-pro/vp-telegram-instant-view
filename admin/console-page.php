74f4bd28ce00925cd98c4fbab65604d58c00aa7c
campusboy <campusboy87@gmail.com>
2018-03-11 11:50:25 +0300

    formatting

--- admin/console-page.php
+++ admin/console-page.php
@@ -1,27 +1,29 @@
 <?php
 add_action( 'admin_menu', 'vp_plus_menu_general_item' );
-function vp_plus_menu_general_item(){
-	add_menu_page( 'VP+ Console', 'VP+ Console', 'manage_options', 'vp-plus-console', 'vp_console_page', plugins_url( 'logo.png', __FILE__ ), 81);
+function vp_plus_menu_general_item() {
+	add_menu_page( 'VP+ Console', 'VP+ Console', 'manage_options', 'vp-plus-console', 'vp_console_page', plugins_url( 'logo.png', __FILE__ ), 81 );
 }
 
-function vp_console_page(){
-	global $vptiv_page;
+function vp_console_page() {
 	?>
-	<div class="wrap">
-		<h2 style="">VP+ Console</h2>
-		<div class="vp-general-menu">
-		Support:
-		<a href="mailto:oleg@valko.pro">oleg@valko.pro</a>
-		<a href="vp-plus.top">vp-plus.top</a>
-		</div>
-	</div>
-	<style>
-		.vp-general-menu {
-			background:#000;color:#fff;padding:10px;
-		}
-		.vp-general-menu a {
-			color: #fff;
-		}
-	</style>
+    <div class="wrap">
+        <h2 style="">VP+ Console</h2>
+        <div class="vp-general-menu">
+            Support:
+            <a href="mailto:oleg@valko.pro">oleg@valko.pro</a>
+            <a href="//vp-plus.top">vp-plus.top</a>
+        </div>
+    </div>
+    <style>
+        .vp-general-menu {
+            background: #000;
+            color: #fff;
+            padding: 10px;
+        }
+
+        .vp-general-menu a {
+            color: #fff;
+        }
+    </style>
 	<?php
 }
